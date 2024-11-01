4410d66a5d91bb54f6397ce2364c8d5fcc3b3dd6
TopGEO <nikanatroshvili10@gmail.com>
2024-11-01 17:59:56 +0400

    First commit, added all visual and the most functional side

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol, isActive }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function changeIsEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" required value={playerName} onChange={handleChange}/>
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={changeIsEditing}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}

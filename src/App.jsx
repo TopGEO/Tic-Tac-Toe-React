a72cebb579e6026cbec8ec93ea548e0d9fb8045a
TopGEO <nikanatroshvili10@gmail.com>
2024-11-03 16:29:19 +0400

    Fix for change name function

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,15 @@ const initialGameBoard = [
 ];
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -27,7 +31,7 @@ function App() {
     gameBoard[row][col] = player;
   }
 
-  const winner = checkForWinner(gameBoard);
+  const winner = checkForWinner(gameBoard, players);
 
   const draw = gameTurns.length === 9 && !winner;
 
@@ -43,14 +47,25 @@ function App() {
     });
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} onChangeName={handlePlayerNameChange}/>
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} onChangeName={handlePlayerNameChange}/>
         </ol>
-        {(winner || draw) && <GameOver winner={winner} onRestart={() => setGameTurns([])}/>}
+        {(winner || draw) && (
+          <GameOver winner={winner} onRestart={() => setGameTurns([])} />
+        )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
